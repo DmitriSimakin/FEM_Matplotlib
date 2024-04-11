7875aa144e9c33d188fff0d2fe3913e5ec579168
Dmitri Simakin <118720205+DmitriSimakin@users.noreply.github.com>
2024-04-11 14:17:57 +0300

    Add files via upload
    init commit

---
+++ Matplotlib: информация + работающие примеры.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+
+'''
+# Библиотека Python MatPlotLib
+'''
+st.image('mpl.png')
+
+st.subheader("Выполнили студенты группы №3:")
+st.subheader("Сортоева Ксения")
+st.subheader("Рыжов Артём")
+st.subheader("Пекин Андрей")
+st.subheader("Симакин Дмитрий")
+st.subheader("")
+st.subheader("")
+
+st.markdown("Cсылка на GitHub :rocket: : ")
